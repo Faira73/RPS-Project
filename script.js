9d8f0e1758360d2d92b596c075d8698ebe32bca2
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-06-15 19:13:27 +0300

    Add getHumanChoice and getMachineChoice and choice functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("11111");
+function getComputerChoice (){ 
+    return choice(Math.floor(Math.random() * 3));
+}
+
+function choice (x){
+    if (x == 0) return 'rock';
+    if (x == 1) return 'paper';
+    if (x == 2) return 'Scissor';
+ }
+
+ function getHumanChoice(){
+    let  userInput = prompt ("Rock, Paper, or Scissor");
+    return choice(userInput); 
+ }
+
+ console.log(getHumanChoice());
