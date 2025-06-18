0266685e89db2362b88adc9a2af851e8d95aeff3
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-06-18 17:53:19 +0300

    Add playRound function

--- script.js
+++ script.js
@@ -5,14 +5,27 @@ function getComputerChoice (){
 function choice (x){
     if (x == 0) return 'rock';
     if (x == 1) return 'paper';
-    if (x == 2) return 'Scissor';
+    if (x == 2) return 'scissor';
  }
 
  function getHumanChoice(){
-    let  userInput = prompt ("Rock, Paper, or Scissor");
-    return choice(userInput); 
+    let  humanChoice = prompt ("Rock, Paper, or Scissor");
+    return choice(humanChoice); 
+ }
+
+ function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    if (humanChoice == computerChoice)
+        { return 'Tie!'}
+    if ((humanChoice == "rock" && computerChoice == "scissor") || 
+        (humanChoice == 'scissor' && computerChoice == 'paper') || 
+        (humanChoice == 'paper' && computerChoice == 'rock'))
+        {return `You win! ${humanChoice} beats ${computerChoice}`;}
+    return `You lose! ${computerChoice} beats ${humanChoice}`;
  }
 
 let humanScore = 0; 
 let computerScore = 0; 
- console.log(getHumanChoice());
+ console.log(playRound());
