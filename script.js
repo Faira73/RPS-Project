75a5358488eea8539ae1f1d42ee938cc75bc2773
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-06-18 17:56:39 +0300

    Add comments

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// Function to generate a random integer for computer choice
 function getComputerChoice (){ 
     return choice(Math.floor(Math.random() * 3));
 }
-
+// Function that converts int inputs to rock, paper, or scissor
 function choice (x){
     if (x == 0) return 'rock';
     if (x == 1) return 'paper';
     if (x == 2) return 'scissor';
  }
-
+// Function to prompt the human to choose
  function getHumanChoice(){
     let  humanChoice = prompt ("Rock, Paper, or Scissor");
     return choice(humanChoice); 
